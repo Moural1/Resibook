3e2887f4ecb67c9dc284caf297796e69732ec783
Igor Augusto Moura Lopes <igormouralopes63@gmail.com>
2026-07-14 16:35:00 -0300

    Adiciona protocolos finais de AVC e melhora navegacao ACLS

--- src/app/acls/page.tsx
+++ src/app/acls/page.tsx
@@ -1,9 +1,9 @@
-import { AclsIntroductionPending, AclsShell } from "@/components/acls-protocol";
+import { AclsOverview, AclsShell } from "@/components/acls-protocol";
 
 export default function AclsPage() {
   return (
     <AclsShell>
-      <AclsIntroductionPending />
+      <AclsOverview />
     </AclsShell>
   );
 }
